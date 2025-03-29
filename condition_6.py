1379858228ee5a48b1577679f876ea2eb45bfee6
Zoey Jones <zoeymjoness@gmail.com>
2025-03-28 20:39:08 -0700

    Your commit message here

--- condition_6.py
+++ condition_6.py
@@ -1,6 +1,8 @@
 import time 
 
 def main():
+
+  # Hard coding values just to have these variables in the code
   recycle_pressure = "moderate"
   bta_pressure = "low"
   btb_pressure = "high"
@@ -8,9 +10,9 @@ def main():
   btb_valve = 50
   fully_open = 100
   fully_closed = 0
-  compressor_speed 
+  compressor_speed = 100
   highest_speed = 100
-  
+
   while recycle_pressure == "moderate" and bta_pressure == "low" and btb_pressure == "high":
     if btb_valve != fully_open:
       btb_valve = fully_open
@@ -21,18 +23,23 @@ def main():
     if bta_valve != fully_closed:
       bta_valve = fully_closed
 
-    original_recycle_pressure = recycle_tank # this is the original pressure in the recycle tank
+    # Original pressure in the recycle tank before changes are made
+    original_recycle_pressure = recycle_pressure
 
-    time.sleep(10) # waiting for effects of changes made above in the pressure recycle tank 
+    # Waiting for pressure to change in the recycle tank 
+    time.sleep(10) 
 
-    pressure_change = recycle_tank - original_recycle_pressure # current recycle pressure minus recycle pressure before changes were made
+    # Finding the change in pressure in the recycle tank
+    pressure_change = recycle_pressure - original_recycle_pressure 
 
-    if pressure_change < 0: # pressure is decreasing in the recycle tank, this is good
-
+    # Check whether the pressure is decreasing in the recycle tank (which is ideal)
+    if pressure_change < 0: 
       time.sleep(10)
                
-    elif pressure_change >= 0 and compressor_speed <= (highest_speed - 5) # pressure is increasing in recycle tank, this is bad
-      compressor_speed = compressor_speed + 5 # increae compressor speed even more if possible 
+    # Pressure is increasing in the recycle tank (this is not ideal)          
+    elif pressure_change >= 0 and compressor_speed <= (highest_speed - 5): 
+      # If possible, increase the compressor speed
+      compressor_speed = compressor_speed + 5 
 
             
 if __name__ =="__main__":
