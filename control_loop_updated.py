8d12f4bf5d63ae4526c116636e9792a081552b85
oliwhitla <oliviawhitelaw24@gmail.com>
2025-04-01 00:05:23 -0700

    Update control_loop_updated.py
    code going out of range oops

--- control_loop_updated.py
+++ control_loop_updated.py
@@ -1,3 +1,5 @@
+# can I use backflow to 
+
 class GasSystem:
     
     # range for max pressure = 2.3 highest (I kind of forgot the values)
@@ -17,7 +19,7 @@ class GasSystem:
         self.bta_volume = bta_volume
         self.btb_volume = btb_volume
 
-        # Control Parameters - Note: Problem where do we start?
+        # Control Parameters - Note: Problem where do we start? 
         self.compressor_speed = 50
         self.valve_BA = 50
         self.valve_BB = 50
@@ -71,28 +73,68 @@ class GasSystem:
     
     
     # update tank volumes 
-    def update_volumes(self, recycling_volume=None, bta_volume=None, btb_volume=None): 
+    def update_volumes(self): 
         
-        if recycling_volume is not None:
-            self.recycling_volume = recycle_input + recycling_volume - ((compressor_speed/max_compressor_speed) * max_recycle_valve_flow)
+        if self.recycling_volume is not None:
+            self.recycling_volume = self.recycle_input + self.recycling_volume - ((self.compressor_speed/ self.max_compressor_speed) * self.max_recycle_valve_flow)
             
-        if bta_volume is not None:
-            self.bta_volume = bta_input + bta_volume - (self.max_buffer_valve_flow  * (valve_BA/100))
+        if self.bta_volume is not None:
+            self.bta_volume = self.bta_input + self.bta_volume - (self.max_buffer_valve_flow  * (self.valve_BA/100))
             
-        if btb_volume is not None:
-            self.btb_volume = btb_input + btb_volume - (self.max_buffer_valve_flow  * (valve_BB/100))
-         
+        if self.btb_volume is not None:
+            self.btb_volume = self.btb_input + self.btb_volume - (self.max_buffer_valve_flow  * (self.valve_BB/100))
     
+    
+    # adjust the volume in BA
+    def adjust_BA(self): 
+        bta_status = self.classify_volume(self.bta_volume)
+        recycle_status = self.classify_volume(self.recycling_volume)
+        
+        if self.bta_volume > self.recycling_volume:
+            
+            # working with simplified conditions for now
+            if bta_status == "high": 
+                self.valve_BA = 100     # open bta valve 100 %
+            elif bta_status == "low": 
+                self.valve_BA = 0
+        else: 
+            print("Closing valve B: Buffer too low to flow")
+            self.valve_BA = 0
+            
+            
+            
+    # adjust the volume in BB
+    def adjust_BB(self): 
+        btb_status = self.classify_volume(self.btb_volume)
+        recycle_status = self.classify_volume(self.recycling_volume)
+        
+        if self.btb_volume > self.recycling_volume:
+            
+            # working with simplified conditions for now 
+            if btb_status == "high": 
+                self.valve_BB = 100     # open bta valve 100 %
+            elif btb_status == "low": 
+                self.valve_BB = 0
+        else: 
+            print("Closing valve B: Buffer too low to flow")
+            self.valve_BB = 0
+                   
+                
+                
+    # adjust volume in recycle compressor 
+    def adjust_recycle(self): 
+        recycle_status = self.classify_volume(self.recycling_volume)
+        
+        if recycle_status == "low": 
+            self.compressor_speed = max(self.compressor_speed - 10, self.lowest_compressor_speed)
+            print("Recycle low → Decreasing compressor speed")
+        elif recycle_status == "high": 
+            self.compressor_speed = min(self.compressor_speed + 10, self.max_compressor_speed)
+            print("Recycle high → Increase compressor speed")
+        
+        
         
     def control_loop(self): 
-                    
-        # logic open valve BA to release pressure
-        if self.valve_BB != 0: 
-            self.valve_BB = 0
-                
-        # logic close valv BB to increase pressure
-        if self.valve_BB != 100: 
-            self.valve_BB = 100
             
         # take the derivative 
         original_pressure = self.recycling_volume
@@ -100,14 +142,19 @@ class GasSystem:
         
         # simulating the volume change in tanks over 10 seconds 
         for _ in range(10): 
-            # read row data from excel 
-            self.parse_data()
-            # update tank volume 
-            self.update_volumes()
+            
+            self.parse_data()       # read row data from excel 
+            self.update_volumes()   # update tank volume 
+            
+            # apply logic here NOTE might change this 
+            self.adjust_recycle()
+            self.adjust_BA()
+            self.adjust_BB()
+            
             self.line_counter += 1
             print(f"[t={self.line_counter}] Recycle: {self.recycling_volume:.3f}, BTA: {self.bta_volume:.3f}, BTB: {self.btb_volume:.3f}, Speed: {self.compressor_speed}, BA: {self.valve_BA}, BB: {self.valve_BB}")
 
-        # actually waiting 10 seconds so it's realistic 
+        # sleep 10 seconds  
         time.sleep(10)
                 
             
@@ -120,10 +167,14 @@ class GasSystem:
             
             # simulating the volume change in tanks over 10 seconds 
             for _ in range(10): 
-            # read row data from excel 
+      
                 self.parse_data()
-                # update tank volume 
-                self.update_volumes()
+                self.update_volumes()   # update tank volume 
+                # apply logic here NOTE might change this 
+                self.adjust_recycle()
+                self.adjust_BA()
+                self.adjust_BB()
+                
                 self.line_counter += 1
                 print(f"[t={self.line_counter}] Recycle: {self.recycling_volume:.3f}, BTA: {self.bta_volume:.3f}, BTB: {self.btb_volume:.3f}, Speed: {self.compressor_speed}, BA: {self.valve_BA}, BB: {self.valve_BB}")
 
@@ -132,8 +183,10 @@ class GasSystem:
 
         
         if derivative >= 0 and self.compressor_speed <= self.max_compressor_speed - 10: 
-            print("Recycle tank stable or rising. Increasing compressor speed.")
-            self.compressor_speed += 10 
+            print("Recycle tank stable or rising. Increasing compressor speed to maintain.")
+            self.compressor_speed += 5
+            
+            # Note decided if I want to update compressor speed here right away 
      
       
         
@@ -144,11 +197,12 @@ class GasSystem:
             recycle_status = self.classify_volume(self.recycling_volume)
             bta_status = self.classify_volume(self.bta_volume)
             btb_status = self.classify_volume(self.btb_volume)
+            print(recycle_status)
+            print(bta_status)
+            print(btb_status)
             
             
-            if recycle_status != "moderate" or bta_status != "low" or btb_status != "high":
-                print(f"\nCondition changed at t={t}.\nRecycle: {recycle_status}, BTA: {bta_status}, BTB: {btb_status}")
-                break
+            print(f"Recycle {t}: {recycle_status}, BTA: {bta_status}, BTB: {btb_status}")
 
             self.control_loop()
         
